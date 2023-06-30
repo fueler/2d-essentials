d085d72c7731ece3002f7631a0945fbe10dd1d34
YusariM <yusarimahubara@proton.me>
2023-06-30 15:58:08 +0100

    defensive programming inside health component for optional timers

--- addons/2d_essentials/health/health_component.gd
+++ addons/2d_essentials/health/health_component.gd
@@ -17,14 +17,20 @@ signal died
 
 enum TYPES {
 	HEALTH,
-	DAMAGE
+	DAMAGE,
+	REGEN
 }
 
 func _ready():
 	health_changed.connect(on_health_changed)
-	invulnerability_timer.timeout.connect(on_invulnerability_timer_timeout)
 	
-	enable_health_regen()
+	if health_regen_timer:
+		health_regen_timer.timeout.connect(on_health_regen_timer_timeout)
+		enable_health_regen()
+		
+	if invulnerability_timer:
+		invulnerability_timer.timeout.connect(on_invulnerability_timer_timeout)
+
 
 func damage(amount: int):
 	if is_invulnerable: amount = 0
@@ -33,51 +39,61 @@ func damage(amount: int):
 	
 	health_changed.emit(amount, TYPES.DAMAGE)
 
-func health(amount: int):
+
+func health(amount: int, type: TYPES = TYPES.HEALTH):
 	current_health = min(max_health, current_health + amount)
-	health_changed.emit(amount,  TYPES.HEALTH)
+	health_changed.emit(amount, type)
+	
 	
 func check_is_death():
 	if current_health == 0:
 		died.emit()
 		owner.queue_free()
 		
+
 func enable_invulnerabiliy(enable: bool, time: float = 0.05):
 	if enable:
 		is_invulnerable = true
-		invulnerability_timer.wait_time = max(0.05, time)
-		invulnerability_timer.one_shot = true
-		invulnerability_timer.start()
+		
+		if invulnerability_timer: 
+			invulnerability_timer.wait_time = max(0.05, time)
+			invulnerability_timer.one_shot = true
+			invulnerability_timer.start()
 	else:
 		is_invulnerable = false
-		invulnerability_timer.stop()
+		
+		if invulnerability_timer:
+			invulnerability_timer.stop()
 
 	invulnerability_changed.emit(enable)
 
 
 func enable_health_regen(amount_per_second: int = health_regen_per_second):
 	health_regen_per_second = amount_per_second
-	health_regen_timer.stop()
 	
-	if health_regen_per_second > 0 and current_health != max_health:
+	if health_regen_timer and health_regen_per_second > 0 and current_health != max_health:
+		health_regen_timer.stop()
+		
 		health_regen_timer.one_shot = false
 		health_regen_timer.wait_time = 1.0
-		health_regen_timer.timeout.connect(on_health_regen_timer_timeout)
 		health_regen_timer.start()
 
 	
 func get_health_percent() -> float:
 	return max(0, current_health / max_health)
 	
+	
 func on_health_changed(amount: int, type: TYPES):
 	if type == TYPES.DAMAGE:
 		Callable(check_is_death).call_deferred()
 
+
 func on_health_regen_timer_timeout():
-	health(health_regen_per_second)
+	health(health_regen_per_second, TYPES.REGEN)
 	
 	if current_health == max_health:
 		health_regen_timer.stop()
+	
 		
 func on_invulnerability_timer_timeout():
 	enable_invulnerabiliy(false)
