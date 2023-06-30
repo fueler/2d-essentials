e0a149eafaf551438e663595b773b61aa87f6d35
YusariM <yusarimahubara@proton.me>
2023-06-30 12:39:21 +0100

    moved folders to the correct place

--- addons/2d_essentials/plugin.gd
+++ addons/2d_essentials/plugin.gd
@@ -5,8 +5,9 @@ const GLOBALS_AUTOLOAD = "Globals2D"
 
 func _enter_tree():
 	add_autoload_singleton(GLOBALS_AUTOLOAD, "res://addons/2d_essentials/autoload/globals.gd")
-	add_custom_type("HealthComponent", "Node2D", preload("res://addons/health/health_component.gd"), preload("res://icon.svg"))
+	add_custom_type("HealthComponent", "Node2D", preload("res://addons/2d_essentials/health/health_component.gd"), preload("res://icon.svg"))
 
 
 func _exit_tree():
 	remove_custom_type("HealthComponent")
+	remove_autoload_singleton(GLOBALS_AUTOLOAD)
