f137d66d10307afc78c0c82d45252fb5cf7eac9c
YusariM <yusarimahubara@proton.me>
2023-08-08 09:54:21 +0100

    removed useless information on README.md and uploaded plugin icon

--- addons/2d_essentials/plugin.gd
+++ addons/2d_essentials/plugin.gd
@@ -6,7 +6,7 @@ const HELPERS_AUTOLOAD = "Helpers"
 func _enter_tree():
 	add_autoload_singleton(HELPERS_AUTOLOAD, "res://addons/2d_essentials/autoload/helpers.tscn")
 	add_custom_type("HealthComponent", "Node2D", preload("res://addons/2d_essentials/survivability/health_component.tscn").get_script(), preload("res://addons/2d_essentials/Backpack.png"))
-	add_custom_type("VelocityComponent2D", "Node2D", preload("res://addons/2d_essentials/movement/velocity_component_2d.tscn").get_script(), preload("res://addons/2d_essentials/Battery.png"))
+	add_custom_type("VelocityComponent2D", "Node2D", preload("res://addons/2d_essentials/movement/velocity_component_2d.tscn").get_script(), preload("res://addons/2d_essentials/Car.png"))
 	add_custom_type("ShakeCameraComponent2D", "Node2D", preload("res://addons/2d_essentials/camera/shake_camera_component.tscn").get_script(), preload("res://addons/2d_essentials/MagnifyingGlass.png"))
 
 
