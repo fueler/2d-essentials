86788a5c3be1c43d9e17f74af18a7974bdae156d
s3r0s4pi3ns <s3r0s4pi3ns@protonmail.com>
2023-08-28 14:09:14 +0100

    setup dash on the godot essentials motion base class

--- addons/2d_essentials/movement/motion/godot_essentials_motion.gd
+++ addons/2d_essentials/movement/motion/godot_essentials_motion.gd
@@ -6,6 +6,9 @@ signal knockback_received(direction: Vector2, power: float)
 signal temporary_speed_started(previous_speed: float, current_speed: float)
 signal temporary_speed_finished
 signal teleported(from: Vector2, to: Vector2)
+signal dashed(position: Vector2)
+signal finished_dash(initial_position: Vector2, final_position: Vector2)
+signal dash_restarted
 
 
 @export_group("Speed")
@@ -24,6 +27,17 @@ signal teleported(from: Vector2, to: Vector2)
 ## In seconds, the amount of time a speed modification will endure
 @export var DEFAULT_TEMPORARY_SPEED_TIME = 3.0
 
+@export_group("Dash")
+## The speed multiplier would be applied to the player velocity on runtime
+@export var DASH_SPEED_MULTIPLIER: float = 1.5
+## The times this character can dash until the cooldown is activated
+@export_range(1, 10, 1, "or_greater") var times_can_dash: int = 1
+## The time it takes for the dash ability to become available again.
+@export var dash_cooldown: float = 1.5
+## The time this dash will be active
+@export var dash_duration: float = 0.3
+## This variable represents whether the character can initiate another dash while already in the dashing state
+@export var can_dash_while_dashing: bool = true
 
 @export_group("Signals")
 ## Emits a signal when the body reaches its maximum speed.
@@ -39,7 +53,18 @@ signal teleported(from: Vector2, to: Vector2)
 var velocity: Vector2 = Vector2.ZERO
 var facing_direction: Vector2 = Vector2.ZERO
 var last_faced_direction: Vector2 = Vector2.RIGHT
+
 var temporary_speed_timer: Timer
+
+var dash_duration_timer: Timer
+var dash_queue: Array[Vector2] = []
+var is_dashing: bool = false:
+	set(value):
+		if value != is_dashing:
+			if value:
+				dashed.emit(body.global_position)
+				
+		is_dashing = value
 
 
 func _get_configuration_warnings() -> PackedStringArray:
@@ -52,8 +77,9 @@ func _get_configuration_warnings() -> PackedStringArray:
 	return warnings
 
 
-func _ready():
-	_create_temporary_speed_timer(DEFAULT_TEMPORARY_SPEED_TIME)
+func ready():
+	_create_temporary_speed_timer()
+	_create_dash_duration_timer()
 
 
 func move() -> void:
@@ -154,6 +180,37 @@ func change_speed_temporary(new_speed: float, time: float = DEFAULT_TEMPORARY_SP
 	return self
 
 
+func has_available_dashes() -> bool:
+	return dash_queue.size() < times_can_dash
+
+
+func can_dash(direction: Vector2 = Vector2.ZERO) -> bool:
+	var available_dashes = not direction.is_zero_approx() and has_available_dashes() 
+	var while_dashing = can_dash_while_dashing or (not can_dash_while_dashing and not is_dashing)
+
+	return available_dashes and while_dashing
+
+
+func dash(target_direction: Vector2 = facing_direction, speed_multiplier: float = DASH_SPEED_MULTIPLIER) -> GodotEssentialsMotion:
+	if can_dash(target_direction):
+		facing_direction = _normalize_vector(target_direction)
+		last_faced_direction = facing_direction
+		is_dashing = true
+		velocity = target_direction * (MAX_SPEED * max(1, absf(speed_multiplier)))
+
+		dash_queue.append(body.global_position)
+		dash_duration_timer.start()
+	
+	
+	return self
+
+
+func reset_dash_queue():
+	if dash_queue.size() > 0:
+		dash_queue.clear()
+		dash_restarted.emit()
+
+
 func _normalize_vector(value: Vector2) -> Vector2:
 	return value if value.is_normalized() else value.normalized()
 
@@ -161,8 +218,33 @@ func _normalize_vector(value: Vector2) -> Vector2:
 func _velocity_has_reached_max_speed() -> bool:
 	return velocity.length_squared() == MAX_SPEED * MAX_SPEED
 
+func _create_dash_duration_timer(time: float = dash_duration):
+	if dash_duration_timer:
+		return
+		
+	dash_duration_timer = Timer.new()
+	dash_duration_timer.process_callback = Timer.TIMER_PROCESS_PHYSICS
+	dash_duration_timer.wait_time = time
+	dash_duration_timer.one_shot = true
+	dash_duration_timer.autostart = false
+	
+	add_child(dash_duration_timer)
+	dash_duration_timer.timeout.connect(on_dash_duration_timer_timeout)
 
-func _create_temporary_speed_timer(time: float) -> void:
+
+func _create_dash_cooldown_timer(time: float = dash_cooldown):
+	var dash_cooldown_timer: Timer = Timer.new()
+
+	dash_cooldown_timer.process_callback = Timer.TIMER_PROCESS_PHYSICS
+	dash_cooldown_timer.wait_time = max(0.05, time)
+	dash_cooldown_timer.one_shot = true
+	dash_cooldown_timer.autostart = true
+	
+	add_child(dash_cooldown_timer)
+	dash_cooldown_timer.timeout.connect(on_dash_cooldown_timer_timeout.bind(dash_cooldown_timer))
+
+
+func _create_temporary_speed_timer(time: float = DEFAULT_TEMPORARY_SPEED_TIME) -> void:
 	if temporary_speed_timer:
 		return
 		
@@ -175,9 +257,20 @@ func _create_temporary_speed_timer(time: float) -> void:
 
 	add_child(temporary_speed_timer)
 	temporary_speed_timer.timeout.connect(on_temporary_speed_timer_timeout)
-	
+
 
 func on_temporary_speed_timer_timeout():
 	MAX_SPEED = original_max_speed
 	temporary_speed_finished.emit()
 
+
+func on_dash_cooldown_timer_timeout(timer: Timer):
+	dash_queue.pop_back()
+	timer.queue_free()
+	
+	
+func on_dash_duration_timer_timeout():
+	is_dashing = false
+	_create_dash_cooldown_timer()
+	finished_dash.emit(dash_queue.back(), body.global_position)
+	
