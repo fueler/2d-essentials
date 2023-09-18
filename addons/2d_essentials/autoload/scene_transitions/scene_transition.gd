74b6468e977223058e329d14e885038d594ea4ea
s3r0s4pi3ns <s3r0s4pi3ns@protonmail.com>
2023-09-18 16:21:48 +0100

    remove the prefab scenes initial approach as the transinioner is based on flexibility

---
+++ addons/2d_essentials/autoload/scene_transitions/scene_transition.gd
@@ -0,0 +1,30 @@
+class_name GodotEssentialsSceneTransition extends Node
+
+signal started_transition(data: Dictionary)
+signal finished_transition(data: Dictionary, next_scene)
+
+func _enter_tree():
+	started_transition.emit(data)
+
+var data: Dictionary = {}
+
+## LOADING GROUP ##
+var progress = []
+var load_status = ResourceLoader.THREAD_LOAD_FAILED
+
+func _ready():
+	if _is_loading_screen():
+		ResourceLoader.load_threaded_request(data["scene_path"])
+		
+		
+func _process(delta):
+	if _is_loading_screen():
+		load_status = ResourceLoader.load_threaded_get_status(data["scene_path"], progress)
+		
+		if load_status == ResourceLoader.THREAD_LOAD_LOADED:
+			finished_transition.emit(data, ResourceLoader.load_threaded_get(data["scene_path"]))
+
+
+func _is_loading_screen() -> bool:
+	return data.has("is_loading_screen") and data["is_loading_screen"]
+
--- addons/2d_essentials/scene_transitions/scene_transition.gd
+++
@@ -1,9 +0,0 @@
-class_name GodotEssentialsSceneTransition extends Node
-
-signal started_transition(data: Dictionary)
-signal finished_transition(data: Dictionary)
-
-
-var data: Dictionary = {}
-
-
